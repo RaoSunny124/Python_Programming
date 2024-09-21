39353fb5e0ffb6c26d56a6c1e96e552084b3a0e1
Hassan <hr8581492gmail.com>
2024-09-21 11:45:03 -0700

    calculator

---
+++ 15 September/function.py
@@ -0,0 +1,31 @@
+def sum(x , y):
+  return x+y
+def subtract(x , y):
+  return x-y  
+def multiply(x , y):
+  return x*y
+def divide(x , y):
+  return x/y    
+print(divide(9 , 3))
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+operator = input("Enter the operator +,-,*,/ :")
+if (operator == '+'):
+    print(sum(number1 , number2))
+elif (operator == "-") :
+    print(subtract(number1 , number2))
+elif  (operator == "*") :
+    print(multiply(number1 , number2))
+elif (operator == "/") :
+    print(divide(number1 , number2))
+else :
+    print('invalid operator')      
+
+
+
+
+
+
+
+
+
--- variables.py
+++ variables.py
@@ -1,3 +1,16 @@
-number1:str = input("Enter the first number:")
-number2:str = input("Enter the second number:")
-operator = input("Enter the operator")
+number1:str = float(input("Enter the first number:"))
+number2:str = float(input("Enter the second number:"))
+operator = input("Enter the operator +,-,*,/ :")
+if (operator == '+'):
+    print(number1 + number2)
+elif (operator == "-") :
+    print(number1 - number2)
+elif  (operator == "*") :
+    print(number1 * number2)
+elif (operator == "/") :
+    print(number1 / number2)
+else :
+    print('invalid operator')      
+
+
+
